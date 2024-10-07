bf3e080172267dd037b6aca313f02f6c4b2b5b15
jy.han <jy.han@baecomm.com>
2024-10-07 16:40:56 +0900

    Initial Commit.

---
+++ src/main/java/com/omnicon/OmniconApplication.java
@@ -0,0 +1,13 @@
+package com.omnicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnicon/OmniconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
